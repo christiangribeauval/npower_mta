e40d00794fd7a67812f849fe0850b68650adc81a
christiangribeauval <57512324+christiangribeauval@users.noreply.github.com>
2019-11-15 14:40:35 -0500

    MTA Practice Files
    All MTA practice files completed so far.
    Pretty sure that's all the C# ones finished.

---
+++ C#/05 - Classes and Objects/A First Class/A First Class/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A_First_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(HappyBirthday.GetMessage+textBox1.Text+"!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            NotBirthday birthdayMessage;
+
+            birthdayMessage = new NotBirthday();
+            birthdayMessage.MyProperty = textBox1.Text;
+            birthdayMessage.presentCount = Int32.Parse(textBox2.Text);
+            MessageBox.Show(birthdayMessage.MyProperty);
+
+
+        }
+    }
+}
---
+++ C#/05 - Classes and Objects/A First Class/A First Class/NotBirthday.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_First_Class
+{
+    class NotBirthday
+    {
+        private string getMessage2(string givenName)
+        {
+            string theMessage;
+
+            theMessage = "It isnt your birthday " + givenName + "!\n";
+            theMessage += "Number of presents = " + NumberOfPresents.ToString();
+
+            return theMessage;
+            
+        }
+        private string birthdayMessage;
+        private int NumberOfPresents;
+
+        public string MyProperty
+        {
+            get { return birthdayMessage; }
+            set { birthdayMessage = getMessage2(value); }
+        }
+
+        public NotBirthday()
+        {
+            NumberOfPresents = 0;
+
+        }
+        public int presentCount
+        {
+            set { NumberOfPresents = value; }
+        }
+    }
+}
